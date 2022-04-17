4b5e9045a70801358d23c7abe4533ccdb138de21
DragonPenguin <98918436+DragonPenguin@users.noreply.github.com>
2022-04-16 19:30:36 -0600

    Update RS232.py

--- RS232.py
+++ RS232.py
@@ -6,11 +6,7 @@
 # The ESP32 MUST BE powered on before being connected to the solar charge controller
 # DO NOT connect the RS232 cable to the ESP Web server while it is powered off
 
-# Startup Sequence of system is
-# 1. ESP32 Wweb server
-# 2. Solar Charge Controller
-
-from machine import Pin, UART
+from machine import UART
 import time
 uart = UART(2, 9600)
 
@@ -21,14 +17,20 @@ def send_battery_bytes() -> float:
     return buf
 
 def send_panel_voltage_bytes():
-    read_panel_voltage = bytes([0x01, 0x03, 0x01, 0x07, 0x00, 0x03, 0xB5, 0xF6])
+    read_panel_voltage = bytes([0x01, 0x03, 0x01, 0x07, 0x00, 0x01, 0x34, 0x37])
     uart.write(read_panel_voltage)
     buf = uart.read()
     return buf
 
 def send_panel_current_bytes():
-    read_panel_current = bytes([0x01, 0x03, 0x01, 0x08, 0x00, 0x02, 0x44, 0x35])
+    read_panel_current = bytes([0x01, 0x03, 0x01, 0x08, 0x00, 0x01, 0x04, 0x34])
     uart.write(read_panel_current)
+    buf = uart.read()
+    return buf
+
+def send_panel_power_bytes():
+    read_panel_power = bytes([0x01, 0x03, 0x01, 0x09, 0x00, 0x01, 0x55, 0xF4])
+    uart.write(read_panel_power)
     buf = uart.read()
     return buf
 
@@ -51,4 +53,11 @@ def get_panel_current():
         temp = send_panel_current_bytes()
         time.sleep(0.1)
     panel_cur = (int.from_bytes(temp[4:5], "big") * 0.01)
+    return panel_cur
+
+def get_panel_power():
+    for x in range(2):
+        temp = send_panel_power_bytes()
+        time.sleep(0.1)
+    panel_cur = (int.from_bytes(temp[4:5], "big"))
     return panel_cur
