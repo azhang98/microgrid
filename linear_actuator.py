50b950637a12f7a6cdc58295c3bac48c838c2631
DragonPenguin <98918436+DragonPenguin@users.noreply.github.com>
2022-04-16 19:28:36 -0600

    Update linear_actuator.py

--- linear_actuator.py
+++ linear_actuator.py
@@ -1,10 +1,5 @@
 import machine
-from time import sleep
 import time
-import time_elapsed
-"""
-def Move_Actuator(position: int):
-    """
 
 Down = machine.Pin(25, machine.Pin.OUT)     # Wire without the blue tape
 Down.off()                                  # Connect to the 5V power supply
@@ -13,22 +8,22 @@ Up.off()
 pos_bottom = machine.Pin(27, machine.Pin.IN, machine.Pin.PULL_UP)       # Top sensor
 pos_top = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)          # Bottom Sensor
 
-cur_pos = 0
+cur_pos = int(0)
 
 def total_travel_time() -> float:           # Measures the time for the linear actuator to go from the bottom hall sensor to the top hall sensor
     while(pos_bottom.value() == 1):
         Down.on()
 
-    start = time_elapsed.elapsed_time_seconds()
+    start = time.time()
     Down.off()
 
     while(pos_top.value() == 1):
         Up.on()
     
-    end = time_elapsed.elapsed_time_seconds()
+    end = time.time()
     Up.off()
 
-    return (end - start)
+    return round((end - start), 3)
 
 def calibrate_actuator():                   # Sets the 
     while(pos_bottom.value() == 1):
@@ -47,19 +42,31 @@ def go_up():
         Up.on()
 
 def move_actuator(new_pos: int) -> None:
+    global cur_pos
     if((new_pos > 100) or (new_pos < 0)):
         print("Position of acuator cannot be greater than 100.")
         return
-    z = (new_pos - cur_pos)/100
+    z = (new_pos - cur_pos)/100 * max_time
+    print(z, " + ", new_pos, " + ", cur_pos)
     cur_pos = new_pos
-    time_range = time.time() + max_time
+    #time_range = time.time() + max_time
     if(z < 0):
-        while (time.time() < ((time_range) * abs(z)) ):
+        now = time.time()
+        while (time.time() < (now + abs(z))):
             if((pos_bottom.value() == 0) or (pos_top.value() == 0)):
+                print("Sensor Detected")
                 Down.off()
+                break
             Down.on()
     else:
-        while (time.time() < ((time_range) * abs(z)) ):
+        now = time.time()
+        while(time.time() < ( now + abs(z) )  ):
             if((pos_bottom.value() == 0) or (pos_top.value() == 0)):
                 Up.off()
+                print("Sensor Detected")
+                break
             Up.on()
+    Down.off()
+    Up.off()
+
+#move_actuator(50)
